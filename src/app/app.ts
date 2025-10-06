40b7d486f15f67be98f9148534615097e9709092
FiglerReNi <renatafigler85@gmail.com>
2025-10-06 20:51:45 +0200

    component, templates, services

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { HeaderComponent} from './header/header.component';
+import { UserComponent } from './user/user.component'
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('first-angular-app');
+}
