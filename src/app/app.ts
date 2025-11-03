044dd2ed412f8119f03740a425468e108d449df7
FiglerReNi <renatafigler85@gmail.com>
2025-11-03 20:58:50 +0100

    component, templates, services

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent} from './header/header.component';
 import { UserComponent } from './user/user.component'
+import { DUMMY_USERS} from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,9 @@ import { UserComponent } from './user/user.component'
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('first-angular-app');
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string){
+    console.log(id);
+    }
 }
