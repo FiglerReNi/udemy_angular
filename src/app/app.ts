8ff93f139cdfaf62bb4cd7fd463dafd707de0e55
FiglerReNi <renatafigler85@gmail.com>
2026-01-06 11:58:25 +0100

    components, templates, services

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,19 @@
-import {Component, Output, signal} from '@angular/core';
+import { Component, Output, signal } from '@angular/core';
 import { HeaderComponent} from './header/header.component';
 import { UserComponent } from './user/user.component'
-import { DUMMY_USERS} from './dummy-users';
-import { TaskComponent } from './task-component/task-component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks-component';
+import { NgFor, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser(){
     return this.users.find(user => user.id === this.selectedUserId);
