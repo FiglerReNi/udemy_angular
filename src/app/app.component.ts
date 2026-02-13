dc36e12013754d013b265947104838f0499800f6
figlerr <renatafigler85@gmail.com>
2026-02-13 14:18:22 +0100

    rxjs - deep dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, inject, DestroyRef, signal, effect, computed} from '@angular/core';
-import {interval, map} from 'rxjs';
+import {interval, map, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -27,24 +27,49 @@ export class AppComponent implements OnInit {
   //   )
   // }
 
-  // SIGNAL
-  clickCount = signal(0);
-  interval = signal(0);
-  doubleInterval = computed(() => this.interval() * 2);
-  constructor(){
-    effect(() => {
-      console.log(`Clicked button ${this.clickCount()} times.`);
+  //CUSTOM OBSERVABLE
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      subscriber.error('');
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value...');
+      subscriber.next({message: 'New value'});
+      timesExecuted++;
+    }, 2000);
+  });
+
+  ngOnInit(): void {
+    this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('Completed!'),
+      error: (err) => console.log(err)
     });
   }
-  ngOnInit() {
-    // ez mindig el fog indulni, akkor is ha semmi nem használja az értéket
-    setInterval(() => {
-      this.interval.update((prevIntervalNumber) => prevIntervalNumber + 1);
-    }, 1000);
-  }
-  onClick(){
-    this.clickCount.update((prevCount) => prevCount + 1)
-  }
+
+  // SIGNAL
+  // clickCount = signal(0);
+  // interval = signal(0);
+  // doubleInterval = computed(() => this.interval() * 2);
+  // constructor(){
+  //   effect(() => {
+  //     console.log(`Clicked button ${this.clickCount()} times.`);
+  //   });
+  // }
+  // ngOnInit() {
+  //   // ez mindig el fog indulni, akkor is ha semmi nem használja az értéket
+  //   setInterval(() => {
+  //     this.interval.update((prevIntervalNumber) => prevIntervalNumber + 1);
+  //   }, 1000);
+  // }
+  // onClick(){
+  //   this.clickCount.update((prevCount) => prevCount + 1)
+  // }
+
 }
 
 /*Observable: value of over time (stream)
