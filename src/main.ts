102eb5f7c1ee577cc4c9b74c06b40f6348364c44
figlerr <renatafigler85@gmail.com>
2026-02-20 16:34:51 +0100

    routers

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { AppComponent } from './app/app.component';
+// import {provideRouter} from "@angular/router";
+// import {TasksComponent} from "./app/tasks/tasks.component";
+// import {routes} from "./app/app.routes";
+import {appConfig} from "./app/app.config";
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+
+// bootstrapApplication(AppComponent, {
+//   providers: [provideRouter(routes)]
+// }).catch((err) => console.error(err));
+
+// bootstrapApplication(AppComponent, {
+//   providers: [provideRouter([{
+//     path: 'tasks', // <your-domain>/tasks
+//     component: TasksComponent
+//   }])]
+// }).catch((err) => console.error(err));
