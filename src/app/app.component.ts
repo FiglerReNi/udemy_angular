87e294eb0797e72fdfd80682cb8bf39c80aa863d
figlerr <renatafigler85@gmail.com>
2026-02-26 16:32:08 +0100

    deferrable views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header/header.component';
-import { UsersComponent } from './users/users.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UsersComponent, RouterOutlet],
+  imports: [WelcomeComponent],
 })
 export class AppComponent {}
-
-
-/* Lazy loading:
-*   Csak azt töltjük be, amire adott oldalon szükség van és nem az egész Angular alkalmazást az indtításkor. Ez performancia,
-*   betöltés gyorsaság szempontjából számít.
-*   Fajtái:
-*     route based lazy loading: azokra az útvonalakra alkalmazható, amelyek nem kellenek az első oldalhoz, hanem pl
-*                               egy gombra kattintással használjuk az útvonalat és a hozzá artozó komponenst.
-*         - egy úvonalra
-*         - összes children útvonalra amik külön file-ban vannak
-*         - servicekre (pl. taskService, amire betöltéskor nincsen szükség, ráérne később buildelni)
-*   deferrable views*/
