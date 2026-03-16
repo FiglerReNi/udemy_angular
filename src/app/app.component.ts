ba7a4cdcf12dc45b58e92d5fe9dcc1c6d667b111
figlerr <renatafigler85@gmail.com>
2026-03-16 12:57:30 +0100

    standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,9 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {AuthService} from "./auth/auth.service";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: false,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: false,
 })
-export class AppComponent implements OnInit{
-  loadedFeature = 'recipe';
-  private authService: AuthService = inject(AuthService)
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
-
-  ngOnInit() {
-    this.authService.autoLogin();
-  }
-}
-
-
-/*https://firebase.google.com/docs/reference/firebase-management/rest*/
+export class AppComponent {}
