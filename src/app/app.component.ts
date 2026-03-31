9fc3a9c02ad588c5499d2b1ac51ddfb79ee26e0b
figlerr <renatafigler85@gmail.com>
2026-03-31 15:50:05 +0200

    pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,146 +1,31 @@
-import {Component} from '@angular/core';
-import {trigger, state, style, transition, animate, keyframes, group} from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: false,
-  animations: [
-    trigger('divState', [
-      state('normal', style({
-        'background-color': 'red',
-        transform: 'translateX(0)'
-      })),
-      state('highlighted', style({
-        'background-color': 'blue',
-        transform: 'translateX(180px)'
-      })),
-      transition('normal <=> highlighted', animate(300)),
-      // transition('highlighted => normal', animate(800))
-    ]),
-    trigger('wildState', [
-      state('normal', style({
-        'background-color': 'red',
-        transform: 'translateX(0)  scale(1)'
-      })),
-      state('highlighted', style({
-        'background-color': 'blue',
-        transform: 'translateX(180px)  scale(1)'
-      })),
-      state('shrunken', style({
-        'background-color': 'green',
-        transform: 'translateX(180px) scale(0.5)'
-      })),
-      transition('normal <=> highlighted', animate(300)),
-      transition('highlighted => normal', animate(800)),
-      // transition('shrunken <=> *', animate(500))
-      transition('shrunken <=> *',[
-        //kiindulo style, amire azonnal vált az induláskor
-        style({
-          'background-color': 'orange',
-        }),
-        // mennyi miliseconds alatt váltson erre a style-ra az előzőhöz képest
-        animate(1000, style({
-          borderRadius: '50px'
-        })),
-        // mennyi idő alatt váltson style nélkülire az előzőhöz képest
-        animate(500)
-      ])
-    ]),
-    trigger('list1', [
-      state('in', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      // a Domban még nem létező (pl mert kattintásra tűnik elő, * - bármilyen állapot is legyen az előtűnés
-      // után a kezdő állapota, ez történjen)
-      transition('void => *', [
-        style({
-          opacity: 0,
-          transform: 'translateX(-100px)'
-        }),
-        animate(300)]),
-      transition('* => void', [
-        animate(300, style({
-          transform: 'translateX(100px)',
-          opacity: 0
-        }))]),
-    ]),
-    trigger('list2', [
-      state('in', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      // a Domban még nem létező (pl mert kattintásra tűnik elő, * - bármilyen állapot is legyen az előtűnés
-      // után a kezdő állapota, ez történjen)
-      transition('void => *', [
-        // keyframes a benne lévő styl-ok száma szerint osztja el az időt és mindegyik ugyanaddig tart.
-        // Ha nem egyenlő arányban akarjuk az offset-el megmondhatjuk melyikre hány % idő jusson
-        animate(1000, keyframes([
-          style({
-            transform: 'translateX(-100px)',
-            opacity: 0,
-            offset: 0
-          }),
-          style({
-            transform: 'translateX(-50px)',
-            opacity: 0.5,
-            offset: 0.3
-          }),
-          style({
-            transform: 'translateX(-20px)',
-            opacity: 1,
-            offset: 0.8
-          }),
-          style({
-            transform: 'translateX(0px)',
-            opacity: 1,
-            offset: 1
-          })
-        ]))
-      ]),
-      transition('* => void', [
-        // a group arra jó, hogy egyszerre fusson le két animáció és ne egymás után
-        group([
-          animate(300, style({
-            color: 'red',
-          })),
-          animate(800, style({
-            transform: 'translateX(100px)',
-            opacity: 0
-          })),
-        ])
-      ]),
-    ])
-  ]
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
-export class AppComponent {
-  state = 'normal';
-  wildState = 'normal';
-  list = ['Milk', 'Sugar', 'Bread'];
+export class AppComponent implements OnInit {
+  posts: Post[] = [];
 
-  onAnimate() {
-    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
-    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
-  }
+  constructor(private http: HttpClient) {}
 
-  onShrink() {
-    this.wildState = 'shrunken';
-  }
-
-  onAdd(item) {
-    this.list.push(item);
-  }
-
-  onDelete(item) {
-    this.list.splice(this.list.indexOf(item), 1);
-  }
-
-  animationStarted(event) {
-    console.log('Start: ' + event);
-  }
-
-  animationDone(event) {
-    console.log('End: ' + event);
+  ngOnInit() {
+    this.http
+      .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(fetchedPosts => (this.posts = fetchedPosts));
   }
 }
+
+
+/* Service workers: Olyan mint egy proxy a http hívás és a frontend között, sok mindenre beállítható.
+* Ami nekünk fontos, hogy tud olyat, hogy cache-el adatokat korábbi http hívásokból, és ha nem érhető
+* el az internet, akkor ebből a cache-ből tud választ adni, tehát internet nélkül is működhet a frontend appunk
+* Telepítés és regisztrálás: ng add @angular/pwa majd: npm run build --production, és ezt nem npm start-el indítuk, hanem
+* telepítjük ezt: npm install -g http-server, a dist folderből tudjuk futtatni:
+* cd dist/angular-pwa majd: http-server -p 8081 így a service-worker-t tudjuk használni
+* A cache működését be kell állítani:*/
