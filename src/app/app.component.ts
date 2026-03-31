0e9bb90d236971a9f6f2eb582f8701057545bdf2
figlerr <renatafigler85@gmail.com>
2026-03-31 11:40:10 +0200

    animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,146 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { init } from './store/counter.actions';
-import { CounterOutputComponent } from './counter-output/counter-output.component';
-import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+import {Component} from '@angular/core';
+import {trigger, state, style, transition, animate, keyframes, group} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: true,
-  imports: [CounterOutputComponent, CounterControlsComponent],
+  standalone: false,
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(180px)'
+      })),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)  scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(180px)  scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(180px) scale(0.5)'
+      })),
+      transition('normal <=> highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      // transition('shrunken <=> *', animate(500))
+      transition('shrunken <=> *',[
+        //kiindulo style, amire azonnal vált az induláskor
+        style({
+          'background-color': 'orange',
+        }),
+        // mennyi miliseconds alatt váltson erre a style-ra az előzőhöz képest
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        // mennyi idő alatt váltson style nélkülire az előzőhöz képest
+        animate(500)
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      // a Domban még nem létező (pl mert kattintásra tűnik elő, * - bármilyen állapot is legyen az előtűnés
+      // után a kezdő állapota, ez történjen)
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))]),
+    ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      // a Domban még nem létező (pl mert kattintásra tűnik elő, * - bármilyen állapot is legyen az előtűnés
+      // után a kezdő állapota, ez történjen)
+      transition('void => *', [
+        // keyframes a benne lévő styl-ok száma szerint osztja el az időt és mindegyik ugyanaddig tart.
+        // Ha nem egyenlő arányban akarjuk az offset-el megmondhatjuk melyikre hány % idő jusson
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        // a group arra jó, hogy egyszerre fusson le két animáció és ne egymás után
+        group([
+          animate(300, style({
+            color: 'red',
+          })),
+          animate(800, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          })),
+        ])
+      ]),
+    ])
+  ]
 })
-export class AppComponent implements OnInit {
-  constructor(private store: Store) {}
+export class AppComponent {
+  state = 'normal';
+  wildState = 'normal';
+  list = ['Milk', 'Sugar', 'Bread'];
 
-  ngOnInit(): void {
-    this.store.dispatch(init());
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
 
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+
+  animationStarted(event) {
+    console.log('Start: ' + event);
+  }
+
+  animationDone(event) {
+    console.log('End: ' + event);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppComponent } from './app/app.component';
-import { provideStore } from '@ngrx/store';
-import { counterReducer } from './app/store/counter.reducer';
-import { provideEffects } from '@ngrx/effects';
-import { CounterEffects } from './app/store/counter.effects';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent, {
-    providers: [provideStore({ counter: counterReducer }), provideEffects([CounterEffects])]
-});
+if (environment.production) {
+  enableProdMode();
+}
 
-/* Ha használjuk az ngrx-et az első lépés, hogy telepítéskor (ez egy külső library) létrehoz magának egy üres store-t.
-* Később ebbe tudunk értéket tenni és onnan kivenni és az értékek változását is kezeli.
-* component <- selector <- store : kifelé irány
-* store <- reducer <- action <- component: befelé irány*/
-/*a service.ts-t válthatjuk ki vele*/
-/*
-Ha beegisztráluk az ngrx-et akkor kell egy reducer, ami a háttérben lévő storhoz hozzáfér és ezen keresztül tudunk bepakolni
-értékeket a store-ba. Értékenként új reducer kell, és be kell regisztrálni a main-be provider-ként valamilyen néven.
-Adatot kivenni a reducer select metódusával tudunk, a main-ben megadott néven tudjuk a reducert meghívni és importálni
-Az action-ban tudnk a service-hez hasonló függvényeket megadni, ami a tényleges adatváltoztatást végzi.
-Az action-t bele kell rakni a reducer-be, hogy minden értékváltozásról értesüljön, és így minden helyen ahol használjuk a reducert a
-selectel a legújabb friss adatot tudjuk használni.
-A selec Observable-t ad vissza amire fel tudunk iratkozni a .html file-okban és így ezek a helyek is egyből lefrissülnek és
-up to datek lesznek
- */
-/*
-Effects:
-side effect-nek hívjuk az olyan eseményeket, amelyek nem közvetlenül a ui-t frissítik. Például a logírás,
-egy http request, vagy locakStorage-be írás. Ezeket nem írhatjuk a reducer-be, azok synchronous működésűek, egyszerűen
-és tisztán kell tartanunk őket.
--> az ngrx-ben ezt oldjuk meg az effects-el
--> telepíteni kell ng add @ngrx/effects
-*/
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
